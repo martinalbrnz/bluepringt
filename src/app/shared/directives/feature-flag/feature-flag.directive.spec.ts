a78cd22faeefbd1910afab022581b9014a359bb5
martinalbrnz <martinalbor96@gmail.com>
2023-06-28 02:06:57 -0300

    Complete tests

--- src/app/shared/directives/feature-flag/feature-flag.directive.spec.ts
+++ src/app/shared/directives/feature-flag/feature-flag.directive.spec.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { FeatureFlagService } from 'src/app/services/feature-flag/feature-flag.service';
+import { FeatureFlagDirective } from './feature-flag.directive';
 
 @Component({
   template: `
-  <div featureFlag="featureA">
+  <div featureFlag="feature">
   </div>
   `
 })
 class TestComponent { }
 
+describe('featureFlagDirective', () => {
+  let service: FeatureFlagService;
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FeatureFlagDirective],
+      declarations: [TestComponent],
+    })
+    service = TestBed.inject(FeatureFlagService);
+  })
+
+  it('should render component with true flag', () => {
+    service.setFeatureFlags({ feature: true })
+    const fixture = TestBed.createComponent(TestComponent)
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement
+    expect(compiled.querySelector('div')).toBeTruthy()
+  })
+
+  it('should not render component with false flag', () => {
+    service.setFeatureFlags({ feature: false })
+    const fixture = TestBed.createComponent(TestComponent)
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement
+    expect(compiled.querySelector('div')).toBeFalsy()
+  })
+
+  it('should not render component with undefined flag', () => {
+    const fixture = TestBed.createComponent(TestComponent)
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement
+    expect(compiled.querySelector('div')).toBeFalsy()
+  })
+})
