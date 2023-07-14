d56bfc93e4cb2f4d0d3d967c30268b021113b6ae
martinalbrnz <martinalbor96@gmail.com>
2023-07-14 13:03:59 -0300

    Add tests to show dev

--- src/app/shared/directives/show-dev/show-dev.directive.spec.ts
+++ src/app/shared/directives/show-dev/show-dev.directive.spec.ts
@@ -1,9 +1,42 @@
-// import { ShowDevDirective } from './show-dev.directive'
+import { Component } from '@angular/core'
+import { TestBed } from '@angular/core/testing'
+import { ShowDevDirective } from './show-dev.directive'
+import { ShowDevService } from './show-dev.service'
 
-// describe('ShowDevDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new ShowDevDirective()
-//     expect(directive)
-//       .toBeTruthy()
-//   })
-// })
+@Component({
+	template: `
+  <div *showDev>
+  </div>
+  `
+})
+class TestComponent { }
+
+describe('ShowDevDirective', () => {
+	let service: ShowDevService
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [ShowDevDirective],
+			declarations: [TestComponent],
+		})
+		service = TestBed.inject(ShowDevService)
+	})
+
+	it('should render component with devMode enabled', () => {
+		service.setDevMode()
+		const fixture = TestBed.createComponent(TestComponent)
+		fixture.detectChanges()
+		const compiled = fixture.nativeElement as HTMLElement
+		expect(compiled.querySelector('div'))
+			.toBeTruthy()
+	})
+
+	it('should not render component with devMode disabled', () => {
+		service.unsetDevMode()
+		const fixture = TestBed.createComponent(TestComponent)
+		fixture.detectChanges()
+		const compiled = fixture.nativeElement as HTMLElement
+		expect(compiled.querySelector('div'))
+			.toBeFalsy()
+	})
+})
