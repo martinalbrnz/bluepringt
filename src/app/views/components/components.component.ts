925fe802c6c8f1a76a2ff736800fe2831ed853fe
martinalbrnz <martinalbor96@gmail.com>
2023-09-27 23:47:50 -0300

    Delete deprecated component

--- src/app/views/components/components.component.ts
+++ src/app/views/components/components.component.ts
@@ -1,31 +1,11 @@
 import { CommonModule } from '@angular/common'
-import { Component, inject } from '@angular/core'
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
-import { FormFieldComponent } from '@components/form-field/form-field.component'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-components',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormFieldComponent,
-    ReactiveFormsModule,
-    FormsModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './components.component.html',
   styleUrls: ['./components.component.scss']
 })
-export default class ComponentsComponent {
-  fb = inject(FormBuilder)
-
-  form = this.fb.group({
-    normal: ['normal', [Validators.required]],
-    errors: ['', [Validators.required, Validators.minLength(10)]],
-    number: [23, []],
-    date: [Date.now(), []],
-    time: [Date.now(), []],
-    selecte_one: [null, [Validators.required]],
-    select_multiple: [[], [Validators.required]],
-
-  })
-}
+export default class ComponentsComponent { }
